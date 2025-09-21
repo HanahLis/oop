d180e2a444129151dbfaf7d48ff6a874b32bf0d4
HanahLis <laladoraema@gmail.com>
2025-09-21 21:08:47 +0700

    Update Introduction.java

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Introduction {
+    public static void main(String[] argv){
+        System.out.print("Nguyễn Thị Ngọc Hà" + "\t" + "24020115" + "\t" + "K69I-IT6");
+        System.out.println("\t" + "HanahLis" + "\t" + "laladoraema@gmail.com");
+        for (int i=9; i>=1; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
 }
